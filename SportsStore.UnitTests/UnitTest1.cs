5bf2265cefa89ad2a14744e7a51fb1d0827bd0b6
popomama <wei_xia@hotmail.com>
2015-06-03 23:54:21 -0400

    updated MVC version

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -4,12 +4,45 @@ using System.Web.Mvc;
 using SportsStore.WebUI.Models;
 using SportsStore.WebUI.HtmlHelpers;
 using SportsStore.WebUI.Models;
+using SportsStore.Domain.Abstract;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace SportsStore.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[] {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                }
+                );
+                        ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            // Act
+            IEnumerable<Product> result =
+            (IEnumerable<Product>)controller.List(2).Model;
+            // Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
         [TestMethod]
         public void Can_Generate_Page_Links()
         {
@@ -22,9 +55,15 @@ namespace SportsStore.UnitTests
                 ItemsPerPage = 10
             };
 
-            Func<int, string> pageUrlDelegate = i => "Page" + 1;
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
 
             MvcHtmlString result = myHelper.PageLinks(pageInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+            result.ToString());
         }
     }
 }
